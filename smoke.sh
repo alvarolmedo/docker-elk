36f1e474b38baab62528d507154f6e36f63eafff
Alvaro Olmedo <aolmedo@airongroup.net>
2017-12-14 23:51:03 +0100

    Smoke test added

---
+++ smoke.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "=========="
+echo "Smoke test"
+echo "=========="
+
+echo Check Kibana
+nc -vz 172.16.0.1 5601
+if [ $? == 0 ];then
+	echo 
+	echo OK
+else
+	echo
+	echo Fail in Kibana
+	exit 1
+fi
+
+
+echo Check Elasticsearch
+nc -vz 172.16.0.1 9200
+if [ $? == 0 ];then
+	echo 
+	echo OK
+else
+	echo
+	echo Fail in Elasticsearch
+	exit 1
+fi
+
+echo Check Kibana
+nc -vz 172.16.0.1 5601
+if [ $? == 0 ];then
+	echo 
+	echo OK
+else
+	echo
+	echo Fail in Kibana
+	exit 1
+fi
